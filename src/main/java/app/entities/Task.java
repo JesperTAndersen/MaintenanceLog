af71790b63c6d6dba085dfbaf77ce6d5fd75b9de
Jesper TA <Axicon89@gmail.com>
2026-02-06 10:36:26 +0100

    Added tasl Entitiy

---
+++ src/main/java/app/entities/Task.java
@@ -0,0 +1,30 @@
+package app.entities;
+
+import app.utils.UserRole;
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDate;
+import java.util.Set;
+
+@Getter
+@NoArgsConstructor
+@Builder
+@ToString
+@EqualsAndHashCode
+@AllArgsConstructor
+@Entity
+public class Task
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "task_id", nullable = false)
+    int taskId;
+
+    @Column(name = "title", nullable = false)
+    String title;
+
+    @Column(name = "description", nullable = false)
+    String description;
+}
+
