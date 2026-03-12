34778a1ed981ce74c6e1bddb677d3a1008588c79
Jesper TA <Axicon89@gmail.com>
2026-03-12 12:08:45 +0100

    Refactored depencyContainer to correctly use interfaces for daos

--- src/main/java/app/config/DependencyContainer.java
+++ src/main/java/app/config/DependencyContainer.java
@@ -5,20 +5,29 @@ import app.controllers.AssetController;
 import app.controllers.MaintenanceLogController;
 import app.controllers.UserController;
 import app.controllers.routes.Routes;
+import app.entities.Asset;
+import app.entities.MaintenanceLog;
+import app.entities.User;
 import app.persistence.daos.AssetDAO;
 import app.persistence.daos.MaintenanceLogDAO;
 import app.persistence.daos.UserDAO;
+import app.persistence.interfaces.IAssetDAO;
+import app.persistence.interfaces.IDAO;
+import app.persistence.interfaces.IMaintenanceLogDAO;
+import app.persistence.interfaces.IUserDAO;
 import app.services.*;
 import jakarta.persistence.EntityManagerFactory;
 
 public class DependencyContainer
 {
     private static final EntityManagerFactory emf = HibernateConfig.getEntityManagerFactory();
-
     //DAOs
-    private final UserDAO userDao;
-    private final AssetDAO assetDao;
-    private final MaintenanceLogDAO logDao;
+    private final IDAO<User> userDao;
+    private final IDAO<Asset> assetDao;
+    private final IDAO<MaintenanceLog> logDao;
+    private final IUserDAO userDaoExpanded;
+    private final IAssetDAO assetDaoExpanded;
+    private final IMaintenanceLogDAO logDaoExpanded;
     //Services
     private final UserService userService;
     private final AssetService assetService;
@@ -31,12 +40,15 @@ public class DependencyContainer
     public DependencyContainer()
     {
         this.userDao = new UserDAO(emf);
+        this.userDaoExpanded = new UserDAO(emf);
         this.assetDao = new AssetDAO(emf);
+        this.assetDaoExpanded = new AssetDAO(emf);
         this.logDao = new MaintenanceLogDAO(emf);
+        this.logDaoExpanded = new MaintenanceLogDAO(emf);
 
-        this.userService = new UserServiceImpl(userDao, userDao);
-        this.assetService = new AssetServiceImpl(assetDao, assetDao);
-        this.logService = new MaintenanceLogServiceImpl(logDao, logDao, assetDao, userDao);
+        this.userService = new UserServiceImpl(userDao, userDaoExpanded);
+        this.assetService = new AssetServiceImpl(assetDao, assetDaoExpanded);
+        this.logService = new MaintenanceLogServiceImpl(logDao, logDaoExpanded, assetDao, userDao);
 
         this.userController = new UserController(userService);
         this.assetController = new AssetController(assetService);
