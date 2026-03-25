fdf8ce212c11ac331075faf318263e3eb0c3c240
Jesper TA <Axicon89@gmail.com>
2026-03-25 15:35:12 +0100

    add welcome endpoint

--- src/main/java/app/controllers/routes/Routes.java
+++ src/main/java/app/controllers/routes/Routes.java
@@ -6,12 +6,14 @@ import app.controllers.EmployeeController;
 import app.controllers.SecurityController;
 import io.javalin.apibuilder.EndpointGroup;
 
+import java.util.Map;
+
+import static io.javalin.apibuilder.ApiBuilder.get;
 import static io.javalin.apibuilder.ApiBuilder.path;
 
 public class Routes
 {
     private static final String API_VERSION = "api/v1";
-
     private final EmployeeRoutes employeeRoutes;
     private final AssetRoutes assetRoutes;
     private final MaintenanceLogRoutes maintenanceLogRoutes;
@@ -29,15 +31,18 @@ public class Routes
     public EndpointGroup getRoutes()
     {
         return () ->
-                path(API_VERSION, () ->
-                {
-                    employeeRoutes.getRoutes().addEndpoints();
-                    assetRoutes.getRoutes().addEndpoints();
-                    maintenanceLogRoutes.getRoutes().addEndpoints();
-                    securityRoutes.getRoutes().addEndpoints();
-                });
-    }
+        {
+            get("/", ctx -> ctx.status(200).json(Map.of("message", "Welcome to the Maintenance Log!")));
 
+            path(API_VERSION, () ->
+            {
+                employeeRoutes.getRoutes().addEndpoints();
+                assetRoutes.getRoutes().addEndpoints();
+                maintenanceLogRoutes.getRoutes().addEndpoints();
+                securityRoutes.getRoutes().addEndpoints();
+            });
+        };
+    }
 
     public static String getApiVersion()
     {
